e58520ead26fe7fa7607582e2ed3e22c767f32c3
Ross Bryson <jacobrbryson@gmail.com>
2025-11-14 08:47:43 -0500

    CICD

--- src/server.js
+++ src/server.js
@@ -14,6 +14,6 @@ const clients = startWebSocketServer(server);
 
 app.use("/", createRouter(clients));
 
-server.listen(config.API_PORT, () => {
+server.listen(8080, () => {
 	console.log("API + WS server running on port 3001");
 });
