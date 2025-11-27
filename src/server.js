3289d19f8334e11c901ca2ced1270ac5a4ea1f48
Ross Bryson <jacobrbryson@gmail.com>
2025-11-27 07:57:25 -0500

    Json paser stuff

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const http = require("http");
-const jsonParser = require("./middleware/jsonParser");
 const createRouter = require("./routes");
 const { startWebSocketServer } = require("./websocket/wsServer");
 const config = require("./config");
 
 const app = express();
-app.use(jsonParser);
+app.use(express.json());
 
 const server = http.createServer(app);
 const clients = startWebSocketServer(server);
